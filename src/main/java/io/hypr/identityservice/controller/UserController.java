7ae778fca16e6d3d5ab78c60ed9798793c4c978b
Hypr <procango2003@gmail.com>
2024-07-20 15:32:55 +0700

    feat(controller): user crud rest api implement

---
+++ src/main/java/io/hypr/identityservice/controller/UserController.java
@@ -0,0 +1,42 @@
+package io.hypr.identityservice.controller;
+
+import io.hypr.identityservice.dto.request.CreateUserRequest;
+import io.hypr.identityservice.dto.request.UpdateUserRequest;
+import io.hypr.identityservice.entity.User;
+import io.hypr.identityservice.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
+public class UserController {
+    private final UserService userService;
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable String id) {
+        return userService.getUserById(id);
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody CreateUserRequest request) {
+        return userService.createUserRequest(request);
+    }
+
+    @PutMapping("/{id}")
+    public User updateUser(@PathVariable String id, @RequestBody UpdateUserRequest request) {
+        return userService.updateUser(id, request);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable String id) {
+        userService.deleteUser(id);
+    }
+}
