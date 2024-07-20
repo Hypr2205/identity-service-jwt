95edc7a00f5baa72264bd4be0b491dda5f2d7977
Hypr <procango2003@gmail.com>
2024-07-20 16:05:08 +0700

    feat: handle method argument not valid

--- src/main/java/io/hypr/identityservice/exception/GlobalExceptionHandler.java
+++ src/main/java/io/hypr/identityservice/exception/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package io.hypr.identityservice.exception;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -8,6 +9,13 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
     @ExceptionHandler(RuntimeException.class)
     ResponseEntity<String> handleRuntimeException(RuntimeException e) {
+        return ResponseEntity
+            .badRequest()
+            .body(e.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    ResponseEntity<String> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
         return ResponseEntity
             .badRequest()
             .body(e.getMessage());
