05eb2da984a9dd4bf702a92dc31c61433ff193d3
Hypr <procango2003@gmail.com>
2024-07-22 01:07:44 +0700

    fix: fix typo

--- src/main/java/io/hypr/identityservice/exception/ErrorCode.java
+++ src/main/java/io/hypr/identityservice/exception/ErrorCode.java
@@ -8,7 +8,7 @@ import lombok.Getter;
 public enum ErrorCode {
     UNCATEGORIZED_EXCEPTION(1000, "Uncategorized exception"),
     USER_EXISTED(1001, "User existed"),
-    USER_NOT_FOUND(1002, "User Not Found"),
+    USER_NOT_FOUND(1002, "User not found"),
     USERNAME_INVALID(1003, "Username can't be blank and must have at least 6 characters"),
     PASSWORD_INVALID(1004, "Password must have at least 8 characters with uppercase & lowercase characters, numbers and 1 special character"),
     ;
