c3286832b9356e1455ed293107931082944866e6
Hypr <procango2003@gmail.com>
2024-07-22 01:05:12 +0700

    feat(repository): declare method to find user by username

--- src/main/java/io/hypr/identityservice/repository/UserRepository.java
+++ src/main/java/io/hypr/identityservice/repository/UserRepository.java
@@ -3,6 +3,10 @@ package io.hypr.identityservice.repository;
 import io.hypr.identityservice.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, String> {
     boolean existsByUsername(String username);
+
+    Optional<User> findByUsername(String username);
 }
