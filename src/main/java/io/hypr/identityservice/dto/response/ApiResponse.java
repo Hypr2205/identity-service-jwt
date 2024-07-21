ad294c34a20dbee37d74ae4b542f283accc52da5
Hypr <procango2003@gmail.com>
2024-07-21 15:45:31 +0700

    feat(api response): not include null value in response

--- src/main/java/io/hypr/identityservice/dto/response/ApiResponse.java
+++ src/main/java/io/hypr/identityservice/dto/response/ApiResponse.java
@@ -1,5 +1,6 @@
 package io.hypr.identityservice.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -9,6 +10,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ApiResponse<T> {
     private int code;
     private String message;
