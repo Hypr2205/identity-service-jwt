c80d87103a4a3ffa0ef45fd17c76bf8cc43bb2db
Hypr <procango2003@gmail.com>
2024-07-21 15:50:28 +0700

    feat(api response): add default response code

--- src/main/java/io/hypr/identityservice/dto/response/ApiResponse.java
+++ src/main/java/io/hypr/identityservice/dto/response/ApiResponse.java
@@ -12,7 +12,7 @@ import lombok.NoArgsConstructor;
 @Builder
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class ApiResponse<T> {
-    private int code;
+    private int code = 200;
     private String message;
     private T result;
 }
