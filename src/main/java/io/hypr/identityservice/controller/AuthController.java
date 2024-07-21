918df6f7803b9fcc5fbd892f102b83f8af430590
Hypr <procango2003@gmail.com>
2024-07-22 01:15:56 +0700

    feat: implement login api

---
+++ src/main/java/io/hypr/identityservice/controller/AuthController.java
@@ -0,0 +1,30 @@
+package io.hypr.identityservice.controller;
+
+import io.hypr.identityservice.dto.request.AuthenticationRequest;
+import io.hypr.identityservice.dto.response.ApiResponse;
+import io.hypr.identityservice.dto.response.AuthenticationResponse;
+import io.hypr.identityservice.service.AuthenticationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+    private final AuthenticationService authenticationService;
+
+    @PostMapping("/login")
+    public ApiResponse<AuthenticationResponse> login(@RequestBody AuthenticationRequest request) {
+        var result = authenticationService.authenticated(request);
+        return ApiResponse
+            .<AuthenticationResponse>builder()
+            .result(AuthenticationResponse
+                .builder()
+                .isAuthenticated(result)
+                .build())
+            .build();
+    }
+}
