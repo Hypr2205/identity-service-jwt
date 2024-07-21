bf36a6925409f7c0f590ce5278c6c11d9e452ccb
Hypr <procango2003@gmail.com>
2024-07-21 16:42:44 +0700

    feat: mapstruct mapper interface

---
+++ src/main/java/io/hypr/identityservice/mapper/UserMapper.java
@@ -0,0 +1,14 @@
+package io.hypr.identityservice.mapper;
+
+import io.hypr.identityservice.dto.request.CreateUserRequest;
+import io.hypr.identityservice.dto.request.UpdateUserRequest;
+import io.hypr.identityservice.entity.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+
+@Mapper(componentModel = "spring")
+public interface UserMapper {
+    User toEntity(CreateUserRequest request);
+
+    void updateUser(@MappingTarget User user, UpdateUserRequest request);
+}
